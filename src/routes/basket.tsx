e39894131282519581ea6c383999fbb07a469f49
signal32 <hdweir@outlook.com>
2026-02-13 11:27:33 +0000

    Add tax lines to basket

--- src/routes/basket.tsx
+++ src/routes/basket.tsx
@@ -1,4 +1,4 @@
-import { Minus, Plus, ShoppingBasketIcon, Trash } from 'lucide-react'
+import { ArrowRightIcon, Minus, Plus, ShoppingBasketIcon, Trash } from 'lucide-react'
 import { Link } from 'react-router'
 import { ContentLayout } from 'src/components/app/BaseLayout'
 import { Button } from 'src/components/ui/button'
@@ -6,18 +6,18 @@ import { Basket, useBasket } from 'src/lib/basket'
 import { cn } from 'src/lib/utils'
 import { Route } from './+types/basket'
 import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from 'src/components/ui/empty'
+import { useMemo } from 'react'
 
 
 export default function Basket({ }: Route.ComponentProps) {
     const basket = useBasket()
 
     return <ContentLayout>
-
         {
             basket.products.size > 0
                 ? <>
-                    <BasketTable basket={basket} />
-                    <Button className='w-full' size='lg'>Proceed to checkout</Button>
+                    <BasketTable />
+                    <Button className='w-full py-5' size='lg'>Proceed to checkout<ArrowRightIcon /></Button>
                 </>
                 : <Empty>
                     <EmptyHeader>
@@ -38,17 +38,34 @@ export default function Basket({ }: Route.ComponentProps) {
                     </EmptyContent>
                 </Empty>
         }
-
-
-
     </ContentLayout>
-
 }
 
+function formatCurrency(amount: number) {
+    return `£${amount.toFixed(2)}`
+}
 
+function BasketTable() {
+    const cellClassName = 'p-1'
+    const basket = useBasket()
 
-function BasketTable(props: { basket: Basket }) {
-    const cellClassName = 'p-2'
+    const calculations = useMemo(() => {
+        const lineItems = basket.products.values().map(({ product, details }) => ({
+            product,
+            details,
+            total: (product.purchase?.price ?? 0) * details.qty
+        })).toArray()
+        const totalExTax = lineItems.reduce((prev, { total }) => prev + total, 0)
+        const totalTax = (totalExTax / 100) * 20
+        const totalIncTax = totalExTax + totalTax
+
+        return {
+            lineItems,
+            totalExTax,
+            totalTax,
+            totalIncTax
+        }
+    }, [basket])
 
     return (
         <table className='table-auto w-full text-lg mb-5'>
@@ -62,18 +79,17 @@ function BasketTable(props: { basket: Basket }) {
                 </tr>
             </thead>
             <tbody>
-                {props.basket.products.values().map(({ product, details }, i) => (
-                    <tr>
+                {calculations.lineItems.map(({ product, details, total }, i) => (
+                    <tr key={i}>
                         <td className={cellClassName}><Link to={`/product/${product.slug}`} className='hover:underline'>{product.name}</Link></td>
-                        <td className={cellClassName}>{product.purchase?.price}</td>
+                        <td className={cellClassName}>{formatCurrency(product.purchase?.price ?? 0)}</td>
                         <td className={cellClassName}>
-                            <Button variant='link' onClick={() => props.basket.updateProduct(product, { ...details, qty: Math.max(1, details.qty - 1) })}><Minus /></Button>
+                            <Button variant='link' onClick={() => basket.updateProduct(product, { ...details, qty: Math.max(1, details.qty - 1) })}><Minus /></Button>
                             {details.qty}
-                            <Button variant='link' onClick={() => props.basket.updateProduct(product, { ...details, qty: details.qty + 1 })}><Plus /></Button>
+                            <Button variant='link' onClick={() => basket.updateProduct(product, { ...details, qty: details.qty + 1 })}><Plus /></Button>
                         </td>
-                        <td className={cellClassName}>{((product.purchase?.price ?? 0) * details.qty).toFixed(2)}</td>
-                        <th className={cellClassName}><Button variant='link' onClick={() => props.basket.removeProduct(product)}><Trash /></Button></th>
-
+                        <td className={cellClassName}>{formatCurrency(total)}</td>
+                        <th className={cellClassName}><Button variant='link' onClick={() => basket.removeProduct(product)}><Trash /></Button></th>
                     </tr>
                 ))}
             </tbody>
@@ -81,8 +97,20 @@ function BasketTable(props: { basket: Basket }) {
                 <tr>
                     <td />
                     <td />
-                    <td className={cn(cellClassName, 'bg-air/50 rounded-l-lg text-center')}>Total:</td>
-                    <td className={cn(cellClassName, 'bg-air/50 rounded-r-lg')}>{props.basket.products.values().reduce((prev, { product, details }) => prev + (product.purchase?.price ?? 0) * details.qty, 0).toFixed(2)}</td>
+                    <td className={cn(cellClassName, 'text-right')}>Total</td>
+                    <td className={cn(cellClassName)}>{formatCurrency(calculations.totalExTax)}</td>
+                </tr>
+                <tr>
+                    <td />
+                    <td />
+                    <td className={cn(cellClassName, 'text-right')}>VAT</td>
+                    <td className={cn(cellClassName)}>{formatCurrency(calculations.totalTax)}</td>
+                </tr>
+                <tr>
+                    <td />
+                    <td />
+                    <td className={cn(cellClassName, 'bg-air text-white rounded-l-md text-right')}>Total (incl. VAT)</td>
+                    <td className={cn(cellClassName, 'bg-air text-white rounded-r-md')}>{formatCurrency(calculations.totalIncTax)}</td>
                 </tr>
             </tfoot>
         </table>
