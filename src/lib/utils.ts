9d1e4a87feeaa717730066ec9b604a81604bd400
signal32 <hdweir@outlook.com>
2026-02-18 19:23:42 +0000

    Add useSyncedRed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from "clsx"
+import { useEffect, useRef } from "react"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
+}
+
+
+export function useSyncedRef<T>(value: T) {
+    const ref = useRef(value)
+    useEffect(() => {
+        ref.current = value
+    }, [value])
+    return ref
 }
