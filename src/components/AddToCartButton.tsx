ee8f384669aa9743c370c0a100fc7ec96ec56caa
signal32 <hdweir@outlook.com>
2026-04-24 14:12:19 +0100

    Remove alert dialog on adding to cart

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -5,7 +5,6 @@ import { useMemo } from "react";
 import { Link } from "react-router";
 import { Config, configId as getConfigId } from "store";
 import { P } from "./common/typography";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "./ui/alert-dialog";
 import { Button } from "./ui/button";
 
 export type OnAddToBasketCb = (config: Config) => Promise<boolean | undefined>
@@ -20,61 +19,40 @@ export function AddToCartButton(props: {
     const configId = useMemo(() => getConfigId(props.config), [props.config])
 
     return storeProduct !== undefined && <>
-        <AlertDialog>
-
-            {basket.order.products[props.product.id]?.configs[configId]
-                ? <div className="flex justify-between items-center bg-green-700/50 p-2 rounded-lg">
-                    <P>In your basket!</P>
-                    <Link to="/basket"><Button variant='outline'>View</Button></Link>
-                </div>
-                : <AlertDialogTrigger asChild>
-                    <Button className="w-full" onClick={async () => {
-                        if ((await props.onAddToBasket?.(props.config)) ?? true)
-                            basket.addProduct(
-                                storeProduct,
-                                props.config
-                            );
-                    }}>Add to basket</Button>
-                </AlertDialogTrigger>
-
-            }
-
-            {Object
-                .values(basket.order.products[props.product.id]?.configs ?? {})
-                .map((config, index) => {
-                    const currentConfigId = getConfigId(config)
-                    return <Link to={`/product/${props.product.slug}?configId=${currentConfigId}`}>
-                        <div
-                            key={index}
-                            className={cn(
-                                "flex justify-between items-center bg-air/30 p-2 my-2 rounded-lg cursor-pointer transition-colors",
-                                "hover:bg-air/75",
-                                configId === currentConfigId ? "bg-air outline-1 outline-primary" : ""
-                            )}
-                        >
-                            <div className="flex flex-col text-sm text-white/80">{Object
-                                .entries(config.options)
-                                .filter(([_, option]) => !option.hidden)
-                                .map(([id, option]) => <p><b className="capitalize">{id}</b>: {option.value}</p>)}
-                            </div>
+        {basket.order.products[props.product.id]?.configs[configId]
+            ? <div className="flex justify-between items-center bg-green-700/50 p-2 rounded-lg">
+                <P>In your basket!</P>
+                <Link to="/basket"><Button variant='outline'>View</Button></Link>
+            </div>
+            : <Button className="w-full" onClick={async () => {
+                if ((await props.onAddToBasket?.(props.config)) ?? true)
+                    basket.addProduct(
+                        storeProduct,
+                        props.config
+                    );
+            }}>Add to basket</Button>
+        }
+        {Object
+            .values(basket.order.products[props.product.id]?.configs ?? {})
+            .map((config, index) => {
+                const currentConfigId = getConfigId(config)
+                return <Link to={`/product/${props.product.slug}?configId=${currentConfigId}`}>
+                    <div
+                        key={index}
+                        className={cn(
+                            "flex justify-between items-center bg-air/30 p-2 my-2 rounded-lg cursor-pointer transition-colors",
+                            "hover:bg-air/75",
+                            configId === currentConfigId ? "bg-air outline-1 outline-primary" : ""
+                        )}
+                    >
+                        <div className="flex flex-col text-sm text-white/80">{Object
+                            .entries(config.options)
+                            .filter(([_, option]) => !option.hidden)
+                            .map(([id, option]) => <p><b className="capitalize">{id}</b>: {option.value}</p>)}
                         </div>
-                    </Link>;
-                })
-            }
-
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>{props.product.name} added!</AlertDialogTitle>
-                    <AlertDialogDescription>
-                        Keep browsing or go to the checkout now?
-                    </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogCancel>Keep browsing</AlertDialogCancel>
-
-                    <AlertDialogAction asChild><Link to={"/basket"}>Go to checkout</Link></AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
+                    </div>
+                </Link>;
+            })
+        }
     </>
 }
