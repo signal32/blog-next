222acd0b7eb3a2bf9af0a1f117a58367eb61e3f7
signal32 <hdweir@outlook.com>
2026-05-24 14:25:12 +0100

    Read shop url env var in server and client contexts

--- src/shop.ts
+++ src/shop.ts
@@ -1,8 +1,17 @@
 import { ShopClient } from "store";
 
-//@ts-expect-error
-process.loadEnvFile()
+let shopUrl
+if (typeof window !== 'undefined') {
+    shopUrl = import.meta.env['VITE_BLOG_SHOP_URL']
+}
+else {
+    //@ts-expect-error
+    process.loadEnvFile()
+    shopUrl = process.env['VITE_BLOG_SHOP_URL']
+}
 
-const shopUrl = process.env['BLOG_SHOP_URL']
-if (!shopUrl) throw new Error('BLOG_SHOP_URL env must be set.')
+if (!shopUrl) {
+    throw new Error('VITE_BLOG_SHOP_URL env must be set.')
+}
+
 export const SHOP = new ShopClient(shopUrl)
